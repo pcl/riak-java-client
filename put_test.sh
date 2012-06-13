a4c6c27f90eb4a68e65c3d75c718a4b82f08db91
Joe DeVivo <joe.devivo@basho.com>
2012-06-13 13:04:13 -0700

    Added an updated version of StateFarm's PUT Script

--- put_test.sh
+++ put_test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-date +%H:%M:%S:%N
+start=`date +%H:%M:%S:%N`
 
 prefix=`date +%s`
 for (( c=1; c<=10000; c++ ))
 do
-  curl  -X PUT -d'{"ClientID":"2","DocumentID":"2" , "ClientAgreementID":"12345678901","DocDescription":"Description of this","CategoryDescription":"This is a catogory description","CustomerNote":"This is a customer Note ", "CreationDate":"mm/dd/yyyy","EffectiveDate":"mm/dd/yyyy", "ExpirrationDate":"mm/dd/yyyy","BillDueDate":"mm/dd/yyyy","AvailableDate":"mm/dd/yyyy","HideShow":"Y", "DocStatusCD":"Y","PolicyDesc":"This is a policy Description","DeliveryChannel":"this is dcnel"}' -H "Content-Type: application/json" -H "x-riak-index-Field1_int: 1" $1/curl_put/$prefix$c  
+  time curl  -X PUT -d'{"ClientID":"2","DocumentID":"2" , "ClientAgreementID":"12345678901","DocDescription":"Description of this","CategoryDescription":"This is a catogory description","CustomerNote":"This is a customer Note ", "CreationDate":"mm/dd/yyyy","EffectiveDate":"mm/dd/yyyy", "ExpirrationDate":"mm/dd/yyyy","BillDueDate":"mm/dd/yyyy","AvailableDate":"mm/dd/yyyy","HideShow":"Y", "DocStatusCD":"Y","PolicyDesc":"This is a policy Description","DeliveryChannel":"this is dcnel"}' -H "Content-Type: application/json" -H "x-riak-index-Field1_int: 1" $1/curl_put/$prefix$c  
 done
- 
-date +%H:%M:%S:%N
+finish=`date +%H:%M:%S:%N`
+echo "Start: " $start
+echo "Finish:" $finish
+
+echo 
+echo "10000 / ($finish - $start) = Transactions Per Second"
